10ecac96f2bd7dc0f58f97d203aa08365969a669
GGrain Development Team <development@ggrain.dev>
2025-11-03 14:50:33 +0100

    git clean

--- backups/random-20251022-112430/src/Core/DotCompute.Core/Execution/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2025 Michael Ivertowski
-// Licensed under the MIT License. See LICENSE file in the project root for license information.
-
-// Global namespace aliases to resolve conflicts between DotCompute.Core.Memory and DotCompute.Abstractions
-global using AbstractionsMemory = DotCompute.Abstractions;
